808f3a1fa472a384c1a52ffd39517cbd60303e59
jgestradam <gregoesme@gmail.com>
2021-05-10 07:34:55 -0500

    interface

---
+++ src/pages/Error404.js
@@ -0,0 +1,8 @@
+const Error404 = () => {
+    const view = /* html */`
+        <div class="Error404">
+            <h2>Error 404</h2>
+        </div>`;
+    return view;
+};
+export default Error404;
